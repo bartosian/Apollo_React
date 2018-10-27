8b82060080d5f6f9202528e7f9ec76b8cb85005e
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-27 18:07:42 -0400

    Created query consumer

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloProvider } from 'react-apollo';
 import './index.css';
-import App from './App';
+import App from './App/index';
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
